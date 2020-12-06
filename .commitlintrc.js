84e8f9d0d0bed1a5e6b8ce2977b49cf19756885a
huibin <bin.hui@convertlab.com>
2020-12-06 18:16:54 +0800

    docs: add commitlint rules introduction

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,7 +8,18 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['fix', 'feat', 'feature', 'docs', 'refactor', 'revert', 'style', 'chore', 'test'],
+      [
+        'build', // 影响构建系统或外部依赖项的更改，如：gulp, broccoli, npm 等
+        'ci', // CI配置文件及脚本的修改
+        'docs', // 文档修改
+        'feat', // 新功能
+        'fix', // bug修复
+        'perf', // 改进性能的代码修改
+        'refactor', // 代码重构，既不修正错误，也不增加功能的修改
+        'revert', // 代码版本还原
+        'style', // 不影响代码的更改，比如空白、格式、缺少分号等
+        'test', // 添加测试或修改测试功能
+      ],
     ],
     // 提交类型大小写
     'type-case': [2, 'always', 'lower-case'],
